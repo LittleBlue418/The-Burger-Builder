59bb76a20e437ddae5184753cd63158cad11eee6
LittleBlue418 <hollyathomas88@gmail.com>
2020-06-03 17:35:07 +0200

    programatically populating burger ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,24 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const Burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map(ingredientName => {
+      const amount = props.ingredients[ingredientName]
+    // Exacutes a function on each element, maps to new array
+      return [...Array(amount)]
+      // make new array with two elements
+        .map((_, i) => {
+        // maps to new array. _ is blank, we are just doing this to get the index
+        return <BurgerIngredient key={ingredientName + i} type={ingredientName} />
+        // use the index to create a unique key
+      });
+    });
+
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
