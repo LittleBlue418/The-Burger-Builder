ac6576b1395fa40ef49bd570c04830d9aa3769b7
LittleBlue418 <hollyathomas88@gmail.com>
2020-06-03 12:25:23 +0200

    code to generate burger image

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import classes from './Layouts.module.css';
+
 import Auxiliary from '../../hoc/Auxiliary';
 
 const Layout = (props) => (
   <Auxiliary>
     <div>Toolbar, SideDraw, Backdrop</div>
-    <main>
+    <main className={classes.content}>
       {props.children}
     </main>
   </Auxiliary>
