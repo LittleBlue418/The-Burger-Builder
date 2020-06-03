c9aad1936807164879d81c6cf2f848fe422ec483
LittleBlue418 <hollyathomas88@gmail.com>
2020-06-03 11:05:13 +0200

    structure for the burger builder elements

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 
 import classes from './App.module.css';
 
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Layout from './components/Layout/Layout';
+
 function App() {
   return (
     <div className={classes.App}>
       <Layout>
-        <p>This is working</p>
+        <BurgerBuilder></BurgerBuilder>
       </Layout>
+
     </div>
   );
 }
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Auxiliary from '../../hoc/Auxiliary';
+
+class BurgerBuilder extends Component {
+  render () {
+      return (
+        <Auxiliary>
+          <div>Burger picture</div>
+          <div>Build Controlls</div>
+        </Auxiliary>
+      );
+  }
+};
+
+export default BurgerBuilder
