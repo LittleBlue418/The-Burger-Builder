dfb09b82e54a64389e26e9951c001249499aba75
LittleBlue418 <hollyathomas88@gmail.com>
2020-06-05 09:05:13 +0200

    Build Toolbar framework

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,10 +3,11 @@ import React from 'react';
 import classes from './Layouts.module.css';
 
 import Auxiliary from '../../hoc/Auxiliary';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const Layout = (props) => (
   <Auxiliary>
-    <div>Toolbar, SideDraw, Backdrop</div>
+    <Toolbar />
     <main className={classes.content}>
       {props.children}
     </main>
