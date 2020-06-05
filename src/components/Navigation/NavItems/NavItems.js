0366d28abb12e20395651c49d6a3f34ba5a4b24f
LittleBlue418 <hollyathomas88@gmail.com>
2020-06-05 10:06:57 +0200

    Building Navbar

---
+++ src/components/Navigation/NavItems/NavItems.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import classes from './NavItems.module.css';
+import NavItem from './NavItem/NavItem';
+
+const NavItems = (props) => (
+  <ul className={classes.NavItems}>
+    <NavItem
+      link="/"
+      active={true}>
+        Burger Builder
+    </NavItem>
+    <NavItem
+      link="/">
+        Checkout
+    </NavItem>
+  </ul>
+)
+
+export default NavItems;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,14 +2,13 @@ import React from 'react';
 import classes from './Toolbar.module.css'
 
 import Logo from '../../Logo/Logo';
+import NavItems from '../NavItems/NavItems';
 
 const Toolbar = (props) => (
   <header className={classes.Toolbar}>
     <div>Menu</div>
     <Logo />
-    <nav>
-      ...
-    </nav>
+    <NavItems />
   </header>
 );
 
