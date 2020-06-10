273cb26e6ddf179997d6c3f5f087c5764dd3948f
LittleBlue418 <hollyathomas88@gmail.com>
2020-06-10 10:39:18 +0200

    Checking if our model updates unnessecarally

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,31 +1,37 @@
-import React from 'react'
+import React, { Component }from 'react'
 import Auxiliary from '../../../hoc/Auxiliary'
 import classes from './OrderSummary.module.css'
 import Button from '../../UI/Button/Button'
 
-const OrderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+  componentDidUpdate() {
+    console.log('[Order summary] Did update')
+  }
+
+  render () {
+    const ingredientSummary = Object.keys(this.props.ingredients)
     .map(igKey => {
       return (
         <li className={classes.OrderItem} key={igKey}>
-          <span className={classes.Capitalize}>{igKey}</span>: {props.ingredients[igKey]}
+          <span className={classes.Capitalize}>{igKey}</span>: {this.props.ingredients[igKey]}
         </li>
       )
     })
 
-  return (
-    <Auxiliary>
-      <h3>Your Order</h3>
-      <p>So far you have:</p>
-      <ul className={classes.OrderList}>
-        {ingredientSummary}
-      </ul>
-      <p><strong>£ {props.price.toFixed(2)}</strong></p>
-      <p>Happy with that?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>Go Back</Button>
-      <Button btnType="Success" clicked={props.purchaseConfirmed}>Checkout</Button>
-    </Auxiliary>
-  )
+    return (
+        <Auxiliary>
+          <h3>Your Order</h3>
+          <p>So far you have:</p>
+          <ul className={classes.OrderList}>
+            {ingredientSummary}
+          </ul>
+          <p><strong>£ {this.props.price.toFixed(2)}</strong></p>
+          <p>Happy with that?</p>
+          <Button btnType="Danger" clicked={this.props.purchaseCancelled}>Go Back</Button>
+          <Button btnType="Success" clicked={this.props.purchaseConfirmed}>Checkout</Button>
+        </Auxiliary>
+    );
+  }
 }
 
 export default OrderSummary
